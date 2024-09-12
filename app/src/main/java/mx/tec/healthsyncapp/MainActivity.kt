0fa3e02c3d6c4c16510836fa2a6b39b6f1ebf7ad
DesireeEspinosa <desireeespinosa245@gmail.com>
2024-09-12 02:27:21 -0600

    Login functionality w/o API

--- app/src/main/java/mx/tec/healthsyncapp/MainActivity.kt
+++ app/src/main/java/mx/tec/healthsyncapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package mx.tec.healthsyncapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,5 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val sharedpref = getSharedPreferences("sesion", MODE_PRIVATE)
+        val usuario = sharedpref.getString("usuario", "#")
+        if (usuario != "#") {
+            val intent = Intent(this@MainActivity, Home::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        binding.btnIniciarSesion.setOnClickListener {
+            val usuario = binding.edtUsuario.text.toString()
+            val contrasena = binding.edtContrasena.text.toString()
+
+            // Por el momento es un usuario cualquiera, falta implementarlo con el API
+            if (usuario == "juan" && contrasena == "hola") {
+                with(sharedpref.edit()){
+                    putString("usuario", usuario)
+                    commit()
+                }
+                val intent = Intent(this@MainActivity, Home::class.java)
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
     }
 }
