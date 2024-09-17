8b1d99bc142653e86f8e69edac51626b4a1ea02f
Carolina Figueroa <andreacarolina.f28@gmail.com>
2024-09-17 12:56:15 -0600

    Activities created

---
+++ app/src/main/java/mx/tec/healthsyncapp/AdminOptions.kt
@@ -0,0 +1,20 @@
+package mx.tec.healthsyncapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdminOptions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin_options)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
