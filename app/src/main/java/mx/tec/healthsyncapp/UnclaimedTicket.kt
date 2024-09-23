4c42eab2c11120a870c14bd5862b72a043157275
DesireeEspinosa <desireeespinosa245@gmail.com>
2024-09-23 01:10:00 -0600

    initial unclaimed ticket layout

---
+++ app/src/main/java/mx/tec/healthsyncapp/UnclaimedTicket.kt
@@ -0,0 +1,20 @@
+package mx.tec.healthsyncapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UnclaimedTicket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_unclaimed_ticket)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
