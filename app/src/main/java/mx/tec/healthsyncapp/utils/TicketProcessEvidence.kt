5fc6fe5de6eec10ce021af92e396a392cf80d273
DesireeEspinosa <desireeespinosa245@gmail.com>
2024-09-30 23:25:52 -0600

    Initial ticket evidence layout

---
+++ app/src/main/java/mx/tec/healthsyncapp/utils/TicketProcessEvidence.kt
@@ -0,0 +1,21 @@
+package mx.tec.healthsyncapp.utils
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import mx.tec.healthsyncapp.R
+
+class TicketProcessEvidence : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ticket_process_evidence)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
